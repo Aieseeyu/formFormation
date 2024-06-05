ecd14af1f6196a4c559b78e36a18898fc03a87af
Stefan R <stefan.dragos3479@gmail.com>
2024-06-05 18:22:21 +0200

    v3 avec debut de step by step et un peu de jquery

--- formulaire.js
+++ formulaire.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
-  let person = { 
-  };
+  let person = {};
+
+  $("#selectOccupation").on("change", function () {
+    person.selected = $("#selectOccupation option:selected").val();
+
+    for (let index = 0; index <= 7; index++) {
+      $("#option" + index).hide();
+    }
+    $("#option" + person.selected).show();
+  });
 
   $("#nextStepTo").on("click", function (e) {
-
     e.preventDefault();
-    
+
     let step = $(this).data("step");
-    $("#section"+step).hide();
+    $("#section" + step).hide();
     step++;
-    $("#section"+step).show();
+    $("#section" + step).show();
     $(this).data("step", step);
 
     return false;
-
   });
 });
