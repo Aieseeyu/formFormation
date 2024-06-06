bef34db66848b8d043a4d9809b1cee39a9b6d035
Stefan R <stefan.dragos3479@gmail.com>
2024-06-06 16:19:23 +0200

    v4 commencement serieux du jquery

--- formulaire.js
+++ formulaire.js
@@ -1,24 +1,46 @@
 $(document).ready(function () {
   let person = {};
+  let helpers = {};
 
-  $("#selectOccupation").on("change", function () {
-    person.selected = $("#selectOccupation option:selected").val();
-
-    for (let index = 0; index <= 7; index++) {
-      $("#option" + index).hide();
+  // fonction pour le stepper d'en haut
+  function stepper(step) {
+    // console.log();
+    $(".section").hide();
+    $(".section" + step).show();
+    for (let index = 4; index > step; index--) {
+      $(".step" + index).removeClass("completed");
     }
-    $("#option" + person.selected).show();
+    for (let index = 1; index <= step; index++) {
+      $(".step" + index).addClass("completed");
+    }
+  }
+
+  $(".step-icon").on("click", function () {
+    // alert($(this).text());
+    stepper($(this).text());
   });
 
+  // pour afficher les pages suivantes
   $("#nextStepTo").on("click", function (e) {
     e.preventDefault();
 
-    let step = $(this).data("step");
-    $("#section" + step).hide();
-    step++;
-    $("#section" + step).show();
-    $(this).data("step", step);
+    helpers.step = $(this).data("step");
+    $(".section" + helpers.step).hide();
+    helpers.step++;
+    stepper(helpers.step);
+    $(".section" + helpers.step).show();
+    $(this).data("step", helpers.step);
 
     return false;
+  });
+
+  // SECTION 1
+  // pour afficher les bons inputs sur le select
+  $("#selectOccupation").on("change", function () {
+    person.selected = $("#selectOccupation option:selected").val();
+    for (let index = 1; index <= 7; index++) {
+      $("#option" + index).hide();
+    }
+    $("#option" + person.selected).show();
   });
 });
