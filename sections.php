6ad4167c066fab8ea790936c2c06936b6b1d5770
Stefan R <stefan.dragos3479@gmail.com>
2024-06-11 16:41:37 +0200

    le validate ne fonctionne pas

--- sections.php
+++ sections.php
@@ -1,27 +1,9 @@
 <?php 
 
-// header("Content-Type: application/json");
+if (isset($_POST["stepHistory"]) && is_numeric($_POST["stepHistory"])) {
 
-$sections = [
-    2 => file_get_contents('sections/section2.html'),
-    3 => file_get_contents('sections/section3.html'),
-    4 => file_get_contents('sections/section4.html'),
-];
+    echo file_get_contents('sections/section'.$_POST["stepHistory"].'.html');
 
-if (isset($_POST["stepHistory"])) {
-    switch (($_POST["stepHistory"])) {
-        case "2":
-            echo $sections[2];
-            break;
-        
-        case "3":
-            echo $sections[3];
-            break;
-        
-        case "4":
-            echo $sections[4];
-            break;
-    }
 }
 
 ?>
